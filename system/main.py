665bd861513bf4f925daa4adc583daa984b0b2db
Gerid <915826939@qq.com>
2023-02-15 17:31:49 +0800

    try work

--- system/main.py
+++ system/main.py
@@ -344,7 +344,7 @@ if __name__ == "__main__":
     parser.add_argument('-ed', "--emb_dim", type=int, default=128)
     parser.add_argument('-alr', "--attn_learning_rate", type=float, default=0.005)
     parser.add_argument('-ncl', "--num_cluster", type=int, default=10)
-    parser.add_argument('-tk', "--tk_ratio", type=float, default=0.8)
+    parser.add_argument('-tk', "--tk_ratio", type=float, default=0.5)
 
 
     args = parser.parse_args()
