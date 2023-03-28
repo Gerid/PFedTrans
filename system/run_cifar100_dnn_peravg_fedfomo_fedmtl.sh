00124c66ca73568df43be23ce33ba2f135b77d0c
Gerid <915826939@qq.com>
2023-03-29 01:50:59 +0800

    fix

--- system/run_cifar100_dnn_peravg_fedfomo_fedmtl.sh
+++ system/run_cifar100_dnn_peravg_fedfomo_fedmtl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nohup python -u main.py -lbs 4 -nc 20 -jr 1 -nb 100 -data Cifar100 -m dnn -algo PerAvg -gr 1000 -did 0 -bt 0.001 -go dnn -wandb True 
+nohup python -u main.py -lbs 4 -nc 20 -jr 1 -nb 100 -data Cifar100 -m dnn -algo PerAvg -gr 1000 -did 0 -bt 0.001 -go dnn -wandb True > cifar100_peravg.out 2>&1 &
 
-nohup python -u main.py -lbs 4 -nc 20 -jr 1 -nb 100 -data Cifar100 -m dnn -algo FedFomo -gr 1000 -M 5 -did 0 -go dnn -wandb True 
+nohup python -u main.py -lbs 4 -nc 20 -jr 1 -nb 100 -data Cifar100 -m dnn -algo FedFomo -gr 1000 -M 5 -did 0 -go dnn -wandb True > cifar100_fedfomo.out 2>&1 &
 
-nohup python -u main.py -lbs 4 -nc 20 -jr 1 -nb 100 -data Cifar100 -m dnn -algo FedMTL -gr 1000 -itk 4000 -did 0 -go dnn -wandb True
+nohup python -u main.py -lbs 4 -nc 20 -jr 1 -nb 100 -data Cifar100 -m dnn -algo FedMTL -gr 1000 -itk 4000 -did 0 -go dnn -wandb True > cifar100_FedMTL.out 2>&1 &
